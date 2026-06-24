51930e24eb7bbd67db4a45973512f0e99233a002
nayaracalenzo <queiroz.14@hotmail.com>
2026-06-23 22:59:15 -0300

    outro exemplo join

--- scriptAula04.sql
+++ scriptAula04.sql
@@ -3,7 +3,7 @@ SELECT categorias_prod.categoria AS categoria, COUNT(*) AS quantidade_produtos F
 produtos INNER JOIN categorias_prod 
 ON produtos.categoria_id = categorias_prod.id GROUP BY categoria ORDER BY categoria ASC;
 
-
+SELECT clientes.nome AS Cliente, compras.id AS Codigo_compra, produtos.nome AS Produtos FROM compras INNER JOIN clientes ON compras.cliente_id = clientes.id INNER JOIN compra_produtos ON compras.id = compra_produtos.compra_id INNER JOIN produtos ON compra_produtos.produto_id = produtos.id WHERE compras.id = 3;
 
 -- exemplo ficticio
 UPDATE produtos SET categoria_id = 1 WHERE nome LIKE '%Corre%';
